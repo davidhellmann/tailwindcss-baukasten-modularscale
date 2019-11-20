7ba3bbb3334bf545e37e0c272f10d878489ade35
David Hellmann <davidhellmann.com@gmail.com>
2019-11-21 00:23:56 +0100

    Update

--- index.js
+++ index.js
@@ -1,37 +1,77 @@
 const ModularScale = require('modularscale-js');
 
 module.exports = function({
-    sizes = [
-        { size: 'sm', value: -1 },
-        { size: 'base', value: 0 },
-        { size: 'lg', value: 1 },
-        { size: 'xl', value: 2 },
-        { size: '2xl', value: 3 },
-        { size: '3xl', value: 4 },
-        { size: '4xl', value: 5 }
+    sizesMobile = [
+        { size: '-2--mobile', value: -2 },
+        { size: '-1--mobile', value: -1 },
+        { size: '0--mobile', value: 0 },
+        { size: '1--mobile', value: 1 },
+        { size: '2--mobile', value: 2 },
+        { size: '3--mobile', value: 3 },
+        { size: '4--mobile', value: 4 },
+        { size: '5--mobile', value: 5 },
+        { size: '6--mobile', value: 6 },
+        { size: '7--mobile', value: 7 },
+        { size: '8--mobile', value: 8 },
+        { size: '9--mobile', value: 9 },
     ],
-    base = 16,
-    ratio = 1.33,
-    unit = 'px',
+    sizesDesktop = [
+        { size: '-2', value: -2 },
+        { size: '-1', value: -1 },
+        { size: '0', value: 0 },
+        { size: '1', value: 1 },
+        { size: '2', value: 2 },
+        { size: '3', value: 3 },
+        { size: '4', value: 4 },
+        { size: '5', value: 5 },
+        { size: '6', value: 6 },
+        { size: '7', value: 7 },
+        { size: '8', value: 8 },
+        { size: '9', value: 9 },
+    ],
+    baseMobile = 16,
+    ratioMobile = 1.1,
+    unitMobile = 'px',
+    baseDesktop = 20,
+    ratioDesktop = 1.25,
+    unitDesktop = 'px',
     variants
 }) {
-    const ms = function(size) {
-        const value = ModularScale(size, { base, ratio });
+    const msMobile = function(sizeMobile) {
+        const value = ModularScale(sizeMobile, { baseMobile, ratioMobile });
 
-        if (unit === 'px') {
-            return Math.round(value) + unit;
+        if (unitMobile === 'px') {
+            return Math.round(value) + unitMobile;
         } else {
-            return value.toFixed(2) + unit;
+            return value.toFixed(2) + unitMobile;
         }
-    }
+    };
+
+    const msDesktop = function(sizeDesktop) {
+        const value = ModularScale(sizeDesktop, { baseDesktop, ratioDesktop });
+
+        if (unitDesktop === 'px') {
+            return Math.round(value) + unitDesktop;
+        } else {
+            return value.toFixed(2) + unitDesktop;
+        }
+    };
 
     return function({ addUtilities }) {
-        const utilities = sizes.map(({ size, value }) => ({
-            [`.ms-${size}`]: {
-                fontSize: ms(value),
+        const mobile = sizesMobile.map(({ sizeMobile, valueMobile }) => ({
+            [`.ms-${sizeMobile}`]: {
+                fontSize: ms(valueMobile),
             },
         }));
 
+        const desktop = sizesDesktop.map(({ sizeDesktop, valueDesktop }) => ({
+            [`.ms-${sizeDesktop}`]: {
+                fontSize: ms(valueDesktop),
+            },
+        }));
+
+        const utilities = [...mobile, ...desktop];
+
         addUtilities(utilities, variants);
     }
-}
+};
