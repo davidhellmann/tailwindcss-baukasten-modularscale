858a3922ffa703537751cbcb6b7f75521f5184f4
David Hellmann <davidhellmann.com@gmail.com>
2019-11-22 01:04:36 +0100

    Fix Bugs

--- index.js
+++ index.js
@@ -2,32 +2,32 @@ const ModularScale = require('modularscale-js');
 
 module.exports = function({
     sizesMobile = [
-        { size: '-2--mobile', value: -2 },
-        { size: '-1--mobile', value: -1 },
-        { size: '0--mobile', value: 0 },
-        { size: '1--mobile', value: 1 },
-        { size: '2--mobile', value: 2 },
-        { size: '3--mobile', value: 3 },
-        { size: '4--mobile', value: 4 },
-        { size: '5--mobile', value: 5 },
-        { size: '6--mobile', value: 6 },
-        { size: '7--mobile', value: 7 },
-        { size: '8--mobile', value: 8 },
-        { size: '9--mobile', value: 9 },
+      { size: '-2--mobile', value: -2 },
+      { size: '-1--mobile', value: -1 },
+      { size: '0--mobile', value: 0 },
+      { size: '1--mobile', value: 1 },
+      { size: '2--mobile', value: 2 },
+      { size: '3--mobile', value: 3 },
+      { size: '4--mobile', value: 4 },
+      { size: '5--mobile', value: 5 },
+      { size: '6--mobile', value: 6 },
+      { size: '7--mobile', value: 7 },
+      { size: '8--mobile', value: 8 },
+      { size: '9--mobile', value: 9 },
     ],
     sizesDesktop = [
-        { size: '-2', value: -2 },
-        { size: '-1', value: -1 },
-        { size: '0', value: 0 },
-        { size: '1', value: 1 },
-        { size: '2', value: 2 },
-        { size: '3', value: 3 },
-        { size: '4', value: 4 },
-        { size: '5', value: 5 },
-        { size: '6', value: 6 },
-        { size: '7', value: 7 },
-        { size: '8', value: 8 },
-        { size: '9', value: 9 },
+      { size: '-2', value: -2 },
+      { size: '-1', value: -1 },
+      { size: '0', value: 0 },
+      { size: '1', value: 1 },
+      { size: '2', value: 2 },
+      { size: '3', value: 3 },
+      { size: '4', value: 4 },
+      { size: '5', value: 5 },
+      { size: '6', value: 6 },
+      { size: '7', value: 7 },
+      { size: '8', value: 8 },
+      { size: '9', value: 9 },
     ],
     baseMobile = 16,
     ratioMobile = 1.1,
@@ -60,13 +60,13 @@ module.exports = function({
     return function({ addUtilities }) {
         const mobile = sizesMobile.map(({ sizeMobile, valueMobile }) => ({
             [`.ms-${sizeMobile}`]: {
-                fontSize: ms(valueMobile),
+                fontSize: msMobile(valueMobile),
             },
         }));
 
         const desktop = sizesDesktop.map(({ sizeDesktop, valueDesktop }) => ({
             [`.ms-${sizeDesktop}`]: {
-                fontSize: ms(valueDesktop),
+                fontSize: msDesktop(valueDesktop),
             },
         }));
 
